41557a60848429cc061ddd0ebf639be535676a7f
Emiliano <milianogomez@gmail.com>
2023-10-26 22:22:50 -0300

    1 comit

--- AppLiga/views.py
+++ AppLiga/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def equipo(self):
+    
+    equipo = Equipo(nombre="San Sebastian", escudo="perro")
+    equipo.save()
+    documentoDeTexto = f"--->Equipo: {equipo.nombre} Escudo: {equipo.escudo}"
+    
+    return HttpResponse(documentoDeTexto)
+
+
